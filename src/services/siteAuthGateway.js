bf0e9006edd46a9e5600bff6bf01df836150de76
Riyan3w <129081548+riyan3110@users.noreply.github.com>
2026-09-12 14:25:15 +0000

    Bump UI asset cache version

--- src/services/siteAuthGateway.js
+++ src/services/siteAuthGateway.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fs = require('node:fs/promises');
 const { createSiteAuth } = require('./siteAuth');
 
-const CACHE_BUST_VERSION = 'cache-20260912d-pullrefresh';
+const CACHE_BUST_VERSION = 'cache-20260912e-ui-final';
 
 function createSiteAuthGateway(innerApp, config) {
   const gateway = express();
